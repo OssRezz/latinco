583faff8e131da3e4bc02a20995c1713517b8e6f
Andres Torres <andres032303@gmail.com>
2022-06-30 11:45:25 -0500

    Add companias y co crud

--- resources/views/co/index.blade.php
+++ resources/views/co/index.blade.php
@@ -15,46 +15,49 @@
 
         <div class="col-12 col-lg-7 mb-3">
             <div class="card shadow-sm">
-                <div class="card-header text-rosado"><i class="fas fa-list text-rosado"></i> <b>Lista de centro de operaciones</b></div>
+                <div class="card-header text-rosado"><i class="fas fa-list text-rosado"></i> <b>Lista de centro de
+                        operaciones</b></div>
                 <div class="card-body">
                     @if ($cos->isEmpty())
-                    <p class="text-center">No hay operaciones registradas actualmente. </p> 
+                        <p class="text-center">No hay operaciones registradas actualmente. </p>
                     @else
-                    <div class="table-responsive">
-                        <table class="table table-bordered nowrap table-hover" style="width: 100%" id="tablaClase">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>centro de operaciones</th>
-                                    <th>Código</th>
-                                    <th>Compañia</th>
-                                    <th class="text-center">Acción</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                @foreach ($cos as $co)
+                        <div class="table-responsive">
+                            <table class="table table-bordered nowrap table-hover" style="width: 100%" id="tablaClase">
+                                <thead>
                                     <tr>
-                                        <td>{{ $co->id}}</td>
-                                        <td>{{ $co->nombre}}</td>
-                                        <td>{{ $co->codigo}}</td>
-                                        <td>{{ $co->compania->nombre}}</td>
-                                        <td class="text-center">
-
-                                            <a class="btn btn-outline-danger btn-table border-0 btn-sm" href="{{ route('admin.co.show', $co->id) }}">
-                                                <i class="fas fa-eye" style="pointer-events: none"></i>
-                                            </a>
-                                            <a class="btn btn-outline-danger btn-table border-0 btn-sm" href="{{ route('admin.co.edit', $co->id) }}">
-                                                <i class="fas fa-edit" style="pointer-events: none"></i>
-                                            </a>
-                                        </td>
+                                        <th>ID</th>
+                                        <th>centro de operaciones</th>
+                                        <th>Código</th>
+                                        <th>Compañia</th>
+                                        <th class="text-center">Acción</th>
                                     </tr>
-                                @endforeach
+                                </thead>
+                                <tbody>
 
-                            </tbody>
-                        </table>
-                    </div>
-                    @endif                    
+                                    @foreach ($cos as $co)
+                                        <tr>
+                                            <td>{{ $co->id }}</td>
+                                            <td>{{ $co->nombre }}</td>
+                                            <td>{{ $co->codigo }}</td>
+                                            <td>{{ $co->compania->nombre }}</td>
+                                            <td class="text-center">
+
+                                                <a class="btn btn-outline-danger btn-table border-0 btn-sm"
+                                                    href="{{ route('admin.co.show', $co->id) }}">
+                                                    <i class="fas fa-eye" style="pointer-events: none"></i>
+                                                </a>
+                                                <a class="btn btn-outline-danger btn-table border-0 btn-sm"
+                                                    href="{{ route('admin.co.edit', $co->id) }}">
+                                                    <i class="fas fa-edit" style="pointer-events: none"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
